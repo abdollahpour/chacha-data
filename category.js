d73ed5a4c0308fe8815d9d161578351d7c114ef4
Hamed Abdollahpour <ha.hamed@gmail.com>
2013-08-16 15:16:48 +0800

    First commit

---
+++ category.js
@@ -0,0 +1,21 @@
+jsonCallback(
+[
+	{
+		"title": "Shoes",
+		"sub":
+			[
+				{"title": "Sport shoes"},
+				{"title": "Boots"},
+				{"title": "Nubuck"}
+			],
+		"icon": "shoes"
+	},
+	{
+		"title": "Bag",
+		"icon": "bag"
+	},
+	{
+		"title": "Other"
+	}
+]
+);
