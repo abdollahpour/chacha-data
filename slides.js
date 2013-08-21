5d59844fab47b2afd3ee993f0ee44222f6ef245d
abdollahpour <ha.hamed@gmail.com>
2013-08-21 12:30:06 +0000

    Fix slide image URLs

--- slides.js
+++ slides.js
@@ -8,25 +8,25 @@ jsonCallback(
 				"id": 1,
 				"url": "http://google.com",
 				"price": "111¥",
-				"link": "https:	//raw.github.com/abdollahpour/chacha-data/master/image/slide/image1.jpg"
+				"https://raw.github.com/abdollahpour/chacha-data/master/image/slides/image1.jpg"
 			},
 			{
 				"id": 2,
 				"url": "http://yahoo.com",
 				"price": "222¥",
-				"link": "https://raw.github.com/abdollahpour/chacha-data/master/image/slide/image2.jpg"
+				"https://raw.github.com/abdollahpour/chacha-data/master/image/slides/image2.jpg"
 			},
 			{
 				"id": 3,
 				"url": "http://bing.com",
 				"price": "333¥",
-				"link": "https://raw.github.com/abdollahpour/chacha-data/master/image/slide/image3.jpg"
+				"https://raw.github.com/abdollahpour/chacha-data/master/image/slides/image3.jpg"
 			},
 			{
 				"id": 4,
 				"url": "http://baidu.com",
 				"price": "444¥",
-				"link": "https://raw.github.com/abdollahpour/chacha-data/master/image/slide/image4.jpg"
+				"https://raw.github.com/abdollahpour/chacha-data/master/image/slides/image4.jpg"
 			}
 		]
 	}
